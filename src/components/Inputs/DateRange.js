5c80c889869720f9d221aa6765b8d98dd343618a
thatblindgeye <thatblindgeye@gmail.com>
2021-05-08 13:34:16 -0400

    Add containers to date range component and make disabled input untabbable

--- src/components/Inputs/DateRange.js
+++ src/components/Inputs/DateRange.js
@@ -5,22 +5,37 @@ export default function DateRange(props) {
   const { dateID, startDate, endDate, enrolled } = props;
 
   return (
-    <div>
-      <label htmlFor={`start-date-${dateID}`}>From</label>
-      <input type='date' id={`start-date-${dateID}`} defaultValue={startDate} />
+    <div className='c-date-range'>
+      <div className='c-date-range__container'>
+        <label htmlFor={`start-date-${dateID}`} className='c-date-range__label'>
+          From
+        </label>
+        <input
+          type='date'
+          id={`start-date-${dateID}`}
+          className='c-date-range__field'
+          defaultValue={startDate}
+        />
+      </div>
 
-      <label htmlFor={`end-date-${dateID}`}>To</label>
-      <input
-        type='date'
-        id={`end-date-${dateID}`}
-        defaultValue={endDate}
-        disabled={enrolled ? true : false}
-      />
-      <Checkbox
-        id={`enrolled-${dateID}`}
-        label='Currently enrolled'
-        fieldClass='c-checkbox__field--large'
-      />
+      <div className='c-date-range__container'>
+        <label htmlFor={`end-date-${dateID}`} className='c-date-range__label'>
+          To
+        </label>
+        <input
+          type='date'
+          id={`end-date-${dateID}`}
+          className='c-date-range__field'
+          defaultValue={endDate}
+          disabled={enrolled ? true : false}
+          tabIndex={enrolled ? '-1' : '0'}
+        />
+        <Checkbox
+          id={`enrolled-${dateID}`}
+          label='Currently enrolled'
+          fieldClass='c-date-range__checkbox c-checkbox__field--large'
+        />
+      </div>
     </div>
   );
 }
